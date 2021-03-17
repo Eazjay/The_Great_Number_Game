32cbf56885713259e196fbfd596401fbcd0e8dbd
MyMac <mymac@Joels-MacBook-Pro.local>
2021-03-17 12:15:06 -0500

    first commit

--- great_number_app/views.py
+++ great_number_app/views.py
@@ -2,38 +2,42 @@ from django.shortcuts import render, redirect
 import random
 
 def index(request):
-    request.session['counter'] = 0
-    num = random.randint(1, 100 + 1)
-    print(num)
+    request.session['guess_count'] = 0
+    request.session['guess_limit'] = 5
+    num = random.randint(1, 100)
+    print(f"Code is {num}")
     request.session['num'] = num
     return render(request, 'index.html')
 
 def guessed_num(request):
-    while request.session['counter'] != 5+1:
+    if request.session['guess_count'] != request.session['guess_limit']:
         if int(request.POST['num']) < request.session['num']:
             context = {
                 "low_num": f"{request.POST['num']} is Too Low!"
             }
-            request.session['counter'] += 1
+            request.session['guess_count'] += 1
+            if request.session['guess_count'] == 5:
+                return render(request, 'try_again.html')
             return render(request, 'low_num.html', context)
         elif int(request.POST['num']) > request.session['num']:
             context = {
                 "high_num": f"{request.POST['num']} is Too High!"
             }
-            request.session['counter'] += 1
+            request.session['guess_count'] += 1
+            if request.session['guess_count'] == request.session['guess_limit']:
+                return render(request, 'try_again.html')
             return render(request, 'high_num.html', context)
         else:
             context = {
                 "correct_num": f"Guessed Right!!! {request.POST['num']} was the correct number!",
             }
-            request.session['counter'] += 1
+            request.session['guess_count'] += 1
             return render(request, 'correct_num.html', context)
-    return render(request, 'try_again.html')
 
 def log_winner(request):
     if 'log' not in request.session:
         request.session['log'] = []
-    request.session['log'].append(f"{request.POST['name']} - Attempted {request.session['counter']} times!")
+    request.session['log'].append(f"{request.POST['name']} - Attempted {request.session['guess_count']} times!")
     request.session.save()
     return redirect('/leader_board')
 
